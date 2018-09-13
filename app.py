c7f3f522bb5fa28b20f1a5ea594485ff657cde11
jl <jonathan.arthur.lock@gmail.com>
2018-09-13 12:23:11 +0200

    v1 - get visable text from any site

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
-from flask import Flask, render_template
+import os
+import operator
+import re
+import nltk
+#from stop_words import stops
+#from collections import Counter
+import re
 
+
+from flask import Flask, render_template, request
+import requests
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
@@ -8,7 +18,19 @@ app = Flask(__name__)
 def scrape():
     errors = []
     results = {}
-    
+    if request.method == "POST":
+        try:
+            url = request.form['urlInput']
+            r = requests.get(url)
+        except:
+            errors.append("Unable to reach URL, please try again")
+            return render_template('index.html', errors=errors)
+        if r:
+            soup = BeautifulSoup(r.text, 'html.parser')
+            for element in soup(['style', 'script', 'head','header', 'title', 'meta', 'footer']):
+                element.extract()
+            results = soup.get_text()
+
     return render_template('index.html', errors=errors, results=results)
 
 
