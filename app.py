c080a06b9dd6ed9d26ce898bc845afe8c069ad5e
jl <jonathan.arthur.lock@gmail.com>
2018-09-13 19:22:48 +0200

    v2 - presents all datapoints

--- app.py
+++ app.py
@@ -1,16 +1,7 @@
-import os
-import operator
-import re
-import nltk
+
 import re
 from nltk import tokenize
-import nltk.classify.util
-from nltk.classify import NaiveBayesClassifier
-from nltk.corpus import subjectivity
-from nltk.sentiment import SentimentAnalyzer
-from nltk.sentiment.util import *
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
-
 from flask import Flask, render_template, request
 import requests
 from bs4 import BeautifulSoup
@@ -42,7 +33,8 @@ def scrape():
             data = []
 
             # Remove too lengthy sentences (understood to be errors)
-            line_list = filter(lambda x: len(str(x)) < 10, tokenized)
+            line_list = list(filter(lambda x: len(str(x)) < 400, tokenized))
+            # line_list = list(filter(lambda x: len(str(x)) > 20, line_list))  use to give min length
 
             # Get polarity of each sentence
             for sentence in line_list:
@@ -54,30 +46,32 @@ def scrape():
                 data[i].update({i: line_list[i]})
 
             # Get num of +/- compound
-
-
+            count_pos = list(filter(lambda x: x['compound'] >= 0, data))
+            count_neg = list(filter(lambda x: x['compound'] < 0, data))
+            count_pos_comp = len(count_pos)
+            count_neg_comp = len(count_neg)
+            count_total = len(data)
 
             # Most/Least values
             most_negative = sorted(data, key=lambda x: x['neg'])
             most_neg = most_negative[((len(data) -1))]
-            neg_comp_sorted = sorted(data, key=lambda x: x['compound'])
-            most_negative_comp = neg_comp_sorted[0]
+            neg_comp_sorted = sorted(data, key=lambda x: x['compound'], reverse=True)
+            most_negative_comp_full = dict(neg_comp_sorted[((len(data)-1))])
+            most_negative_comp_reduced = dict(neg_comp_sorted[((len(data)-1))])
+            for key in ['neg', 'neu', 'pos', 'compound']:
+                most_negative_comp_reduced.pop(key)
+
             most_positive = sorted(data, key=lambda x: x['pos'])
-            most_pos = most_positive[((len(data)-1))]
+            most_pos = most_positive[((len(data) -1))]
             pos_comp_sorted = sorted(data, key=lambda x: x['compound'])
-            most_positive_comp = pos_comp_sorted[((len(data)-1))]
+            most_positive_comp_full = dict(pos_comp_sorted[((len(data)-1))])
+            most_positive_comp_reduced = dict(pos_comp_sorted[((len(data)-1))])
+            for key in ['neg', 'neu', 'pos', 'compound']:
+                most_positive_comp_reduced.pop(key)
 
-            # Return most positive, most negative
+
     return render_template('index.html', **locals())
-                #for k in sorted(ss):
 
-                    # print('{0}: {1}, '.format(k, ss[k]), end='')
-
-"""
-            def word_feats(raw_result_text):
-                return dict([(word, True) for word in raw_result_text])
-
-           """
 
 if __name__ == '__main__':
     app.run()
