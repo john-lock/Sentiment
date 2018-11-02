aa17884a7e7455dafcae23fe33a7012084e0d627
jl <jonathan.arthur.lock@gmail.com>
2018-11-02 14:43:44 +0100

    tests added

--- tests.py
+++ tests.py
@@ -7,22 +7,39 @@ class Test(unittest.TestCase):
     def setUp(self):
         pass
 
-    def testpage(self):
+    def testPage(self):
+        """
+        Test page is shortened version of a star trek script (source = http://chakoteya.net/NextGen/244.htm )
+        """
         url = 'https://sentiment.johnlock.nl/testpage'
         response = requests.get(url)
         self.assertEqual(response.status_code, 200)
 
-    def testscore(self):
+    def testScore(self):
         with app.test_client() as client:
             sent = {'urlInput': 'https://sentiment.johnlock.nl/testpage'}
             result = client.post('/', data=sent)
-            print(result.data)
-            self.assertIn("Most negative: NEIL: No.", str(result.data))
+            self.assertIn('<div class="progress-bar bg-success" style="width:23.0%">', str(result.data))
+            self.assertIn('', str(result.data))
+
+    def testQuotes(self):
+        with app.test_client() as client:
+            sent = {'urlInput': 'https://sentiment.johnlock.nl/testpage'}
+            result = client.post('/', data=sent)
+            self.assertIn("Most negative:</b> PICARD: No.\\n            <br>Score: -0.296", str(result.data))
+
+    def testTable(self):
+        with app.test_client() as client:
+            sent = {'urlInput': 'https://sentiment.johnlock.nl/testpage'}
+            result = client.post('/', data=sent)
+            self.assertIn("<td>      Starship Mine Stardate: 46682.4 Original Airdate: 29 Mar, 1993", str(result.data))
+
+    def testChart(self):
+        with app.test_client() as client:
+            sent = {'urlInput': 'https://sentiment.johnlock.nl/testpage'}
+            result = client.post('/', data=sent)
+            self.assertIn("0.0, 0.4019, 0.7269, 0.0, 0.4576, 0.0, 0.4019, 0.0, 0.0, 0.4019, -0.1531,", str(result.data))
 
 
 if __name__ == '__main__':
     unittest.main()
-
-
-# test can get scores
-# test front end if possible
