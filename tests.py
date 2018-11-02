ba52b9fb86baba0ef84ea59c19b663b2d8a1f1cb
jl <jonathan.arthur.lock@gmail.com>
2018-11-02 15:05:51 +0100

    fixed test

--- tests.py
+++ tests.py
@@ -20,25 +20,25 @@ class Test(unittest.TestCase):
             sent = {'urlInput': 'https://sentiment.johnlock.nl/testpage'}
             result = client.post('/', data=sent)
             self.assertIn('<div class="progress-bar bg-success" style="width:23.0%">', str(result.data))
-            self.assertIn('', str(result.data))
+            self.assertIn('Total sentences found:</b> 100\\n            <b>Positive:</b> 23', str(result.data))
 
     def testQuotes(self):
         with app.test_client() as client:
             sent = {'urlInput': 'https://sentiment.johnlock.nl/testpage'}
             result = client.post('/', data=sent)
-            self.assertIn("Most negative:</b> PICARD: No.\\n            <br>Score: -0.296", str(result.data))
+            self.assertIn('Most negative:</b> PICARD: No.\\n            <br>Score: -0.296', str(result.data))
 
     def testTable(self):
         with app.test_client() as client:
             sent = {'urlInput': 'https://sentiment.johnlock.nl/testpage'}
             result = client.post('/', data=sent)
-            self.assertIn("<td>      Starship Mine Stardate: 46682.4 Original Airdate: 29 Mar, 1993", str(result.data))
+            self.assertIn('<td>      Starship Mine Stardate: 46682.4 Original Airdate: 29 Mar, 1999', str(result.data))
 
     def testChart(self):
         with app.test_client() as client:
             sent = {'urlInput': 'https://sentiment.johnlock.nl/testpage'}
             result = client.post('/', data=sent)
-            self.assertIn("0.0, 0.4019, 0.7269, 0.0, 0.4576, 0.0, 0.4019, 0.0, 0.0, 0.4019, -0.1531,", str(result.data))
+            self.assertIn('0.0, 0.4019, 0.7269, 0.0, 0.4576, 0.0, 0.4019, 0.0, 0.0, 0.4019, -0.1531,', str(result.data))
 
 
 if __name__ == '__main__':
