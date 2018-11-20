34ad59f6844f434055f89bd6f1230ba3b2e4aa65
jl <jonathan.arthur.lock@gmail.com>
2018-11-20 17:21:32 +0100

    readme added

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
+Analyse the sentiment of each sentence on a page. This application takes any URL and scrapes the text using Beautiful Soup. Text in areas which are not visible to the user, eg within Meta tags, are removed as to not include hidden text in the analysis. 
+
+The resulting text is then broken down into sentences, known as Tokenizing, and passed to Natural Language ToolKit (NLTK) which returns a score for each with the 'Sentiment Intensity Analyzer' it provides. Each sentence score has a positive, neutral and negative component based on each word in the sentence. To give the user a quick insight into the results the sentence with the highest positive score is shown and the same for the negative. A compound score is also returned which is a weighted score based on the positive, neutral and negative composition - developments for this application would be right to utilise this. 
+
+The testing strategy for this application includes using a static page (test.html) to feed into NLPK via the application to ensure results are as expected.
 
--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from bs4 import BeautifulSoup
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
 def analysis():
     """
     Returns a sentiment score for page, and scores for each sentence visible.
@@ -17,28 +17,23 @@ def analysis():
     results = {}
     if request.method == "POST":
         try:
-            url = request.form['urlInput']
+            url = request.form["urlInput"]
             r = requests.get(url)
         except requests.exceptions.MissingSchema as e:
-            url = "http://" + request.form['urlInput']
+            url = "http://" + request.form["urlInput"]
             r = requests.get(url)
         except requests.exceptions.ConnectionError as e:
             errors.append("Unable to reach URL, please try again")
             errors.append("URL attempted:" + url)
-            errors_str = '\n'.join(errors)
-            return render_template('index.html', errors=errors, url=url)
+            errors_str = "\n".join(errors)
+            return render_template("index.html", errors=errors, url=url)
 
         if r:
             # Remove elements that are not visible on page as rendered to user
-            soup = BeautifulSoup(r.text, 'html.parser')
-            for element in soup(['style',
-                                 'script',
-                                 'head',
-                                 'header',
-                                 'title',
-                                 'meta',
-                                 'footer',
-                                 ]):
+            soup = BeautifulSoup(r.text, "html.parser")
+            for element in soup(
+                ["style", "script", "head", "header", "title", "meta", "footer"]
+            ):
                 element.extract()
             raw_result_text = soup.get_text()
             raw_result_text = re.sub("\n", " ", str(raw_result_text))
@@ -58,27 +53,30 @@ def analysis():
 
             # Add sentence id and sentence
             for i in range(len(line_list)):
-                data[i].update({'id': i})
-                data[i].update({'text': line_list[i]})
+                data[i].update({"id": i})
+                data[i].update({"text": line_list[i]})
 
             # Get most positive/negative sentences
             try:
-                most_neg_sorted = sorted(data, key=lambda x: x['neg'])
-                most_neg_txt = most_neg_sorted[((len(data) - 1))]['text']
-                most_neg_id = int(most_neg_sorted[((len(data) - 1))]['id'])
-                most_pos_sorted = sorted(data, key=lambda x: x['pos'])
-                most_pos_txt = most_pos_sorted[((len(data) - 1))]['text']
-                most_pos_id = int(most_pos_sorted[((len(data) - 1))]['id'])
+                most_neg_sorted = sorted(data, key=lambda x: x["neg"])
+                most_neg_txt = most_neg_sorted[((len(data) - 1))]["text"]
+                most_neg_id = int(most_neg_sorted[((len(data) - 1))]["id"])
+                most_pos_sorted = sorted(data, key=lambda x: x["pos"])
+                most_pos_txt = most_pos_sorted[((len(data) - 1))]["text"]
+                most_pos_id = int(most_pos_sorted[((len(data) - 1))]["id"])
 
             except IndexError as e:
-                errors.append("Did not find visible text on page, please try a different page")
-                return render_template('index.html', errors=errors, url=url)
+                errors.append(
+                    "Did not find visible text on page, please try a different page"
+                )
+                return render_template("index.html", errors=errors, url=url)
 
             # Get number of +/- of compound rating
-            count_pos = list(filter(lambda x: x['compound'] >= 0.2, data))
-            count_neut = list(filter(lambda x: x['compound'] > -0.2 and
-                                     x['compound'] < 0.2, data))
-            count_neg = list(filter(lambda x: x['compound'] <= -0.2, data))
+            count_pos = list(filter(lambda x: x["compound"] >= 0.2, data))
+            count_neut = list(
+                filter(lambda x: x["compound"] > -0.2 and x["compound"] < 0.2, data)
+            )
+            count_neg = list(filter(lambda x: x["compound"] <= -0.2, data))
             count_pos_comp = len(count_pos)
             count_neut_comp = len(count_neut)
             count_neg_comp = len(count_neg)
@@ -89,19 +87,19 @@ def analysis():
                 count_neg_perc = round(count_neg_comp / count_total * 100, 2)
 
             # Scores of most neg & pos sentences
-            most_neg_score = data[(most_neg_id)]['compound']
-            most_pos_score = data[(most_pos_id)]['compound']
+            most_neg_score = data[(most_neg_id)]["compound"]
+            most_pos_score = data[(most_pos_id)]["compound"]
 
-    return render_template('index.html', **locals())
+    return render_template("index.html", **locals())
 
 
-@app.route('/testpage')
+@app.route("/testpage")
 def test():
     """
     A static page to test against for consistency
     """
-    return render_template('test.html')
+    return render_template("test.html")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=False)
